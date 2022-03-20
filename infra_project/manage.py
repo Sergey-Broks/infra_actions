0b3b66c4c3537aba9a072313411b20f9431b9209
Sergey-Broks <sergey.broks@yandex.ru>
2022-03-20 09:05:18 +0100

    first

--- infra_project/manage.py
+++ infra_project/manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+print('echo')
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'infra_project.settings')
